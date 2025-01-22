3206c1098434d7d9e8113cb6a39fc08c970f5a20
GB-25 <gior.bozza@studenti.unina.it>
2025-01-22 16:38:10 +0100

    Creazione delle prime classi del progetto
    Creazione della classe Controller nel pacchetto Class e della classe FinestraLogin nel pacchetto GUI

---
+++ INGSW_2024_25/src/Class/Controller.java
@@ -0,0 +1,22 @@
+package Class;
+
+//import 
+import GUI.*;
+
+public class Controller {
+
+	//frame
+	
+	
+	//connessione al db
+	//private final static String url 
+	//private final static String user
+	//private final static String password
+	
+	//costruttore
+	public Controller() {}
+	
+	
+	public static void main(String[] args)
+	{}
+}
---
+++ INGSW_2024_25/src/GUI/FinestraLogin.java
@@ -0,0 +1,13 @@
+package GUI;
+
+//import
+import Class.Controller;
+
+public class FinestraLogin {
+
+	//costruttore
+	public FinestraLogin(Controller C) {
+		
+	}
+	
+}
