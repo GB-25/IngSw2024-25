1de4fb88fe1fc19e322a7855a51fbfd326089bb5
GB-25 <gior.bozza@studenti.unina.it>
2025-01-23 12:54:09 +0100

    migliorie in FinestraLogin
    Reso più "visivamente accettabile" la finestra (si può fare di meglio);
    Aggiunto il pulsante per l'accesso con Google da implementare poi con la rispettiva api (scalare meglio l'immagine)

--- INGSW_2024_25/src/GUI/FinestraLogin.java
+++ INGSW_2024_25/src/GUI/FinestraLogin.java
@@ -42,7 +42,7 @@ public class FinestraLogin extends JFrame {
 		setBackground(new Color(0, 153, 255));
 		setTitle("DietiEstates25");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 621, 390);
+		setBounds(100, 100, 621, 527);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255, 255, 255));
 		contentPane.setBorder(null);
@@ -52,9 +52,9 @@ public class FinestraLogin extends JFrame {
 		JTextPane txtpnP = new JTextPane();
 		txtpnP.setForeground(new Color(0, 153, 255));
 		txtpnP.setFont(new Font("FreeSerif", Font.BOLD, 26));
-		txtpnP.setBounds(305, 25, 292, 198);
+		txtpnP.setBounds(305, 25, 292, 73);
 		txtpnP.setBackground(new Color(255, 255, 255));
-		txtpnP.setText("            Benvenuto su\n           DietiEstates25\n\n        La casa dei tuoi\n                  sogni...\n      A portata di mano!");
+		txtpnP.setText("            Benvenuto su\n           DietiEstates25");
 		contentPane.add(txtpnP);
 		
 		JLabel imageLabel = new JLabel();
@@ -70,13 +70,46 @@ public class FinestraLogin extends JFrame {
         	}
         });
         btnIscriviti.setForeground(new Color(0, 153, 255));
-        btnIscriviti.setBounds(102, 274, 117, 25);
+        btnIscriviti.setBounds(420, 203, 117, 25);
         contentPane.add(btnIscriviti);
         
         JButton btnAccedi = new JButton("Accedi");
         btnAccedi.setForeground(new Color(0, 153, 255));
-        btnAccedi.setBounds(393, 274, 117, 25);
+        btnAccedi.setBounds(420, 285, 117, 25);
         contentPane.add(btnAccedi);
+        
+        JTextPane txtpnSeiGiIscritto = new JTextPane();
+        txtpnSeiGiIscritto.setFont(new Font("FreeSerif", Font.BOLD, 16));
+        txtpnSeiGiIscritto.setForeground(new Color(0, 153, 255));
+        txtpnSeiGiIscritto.setText("Sei già iscritto?");
+        txtpnSeiGiIscritto.setBounds(430, 250, 144, 21);
+        contentPane.add(txtpnSeiGiIscritto);
+        
+        JTextPane txtpnLaCasaDei = new JTextPane();
+        txtpnLaCasaDei.setFont(new Font("FreeSerif", Font.BOLD, 26));
+        txtpnLaCasaDei.setForeground(new Color(0, 153, 255));
+        txtpnLaCasaDei.setText("La casa dei tuoi sogni...");
+        txtpnLaCasaDei.setBounds(337, 95, 300, 35);
+        contentPane.add(txtpnLaCasaDei);
+        
+        JTextPane txtpnaPortataDi = new JTextPane();
+        txtpnaPortataDi.setForeground(new Color(0, 153, 255));
+        txtpnaPortataDi.setFont(new Font("FreeSerif", Font.BOLD, 26));
+        txtpnaPortataDi.setText("...A portata di mano!");
+        txtpnaPortataDi.setBounds(347, 142, 262, 49);
+        contentPane.add(txtpnaPortataDi);
+        
+        ImageIcon iconGoogle = new ImageIcon(getClass().getResource("/immagini/Google_logo.svg.png"));
+        Image imgGoogle = iconGoogle.getImage();
+        Image imgGoogleScaled = imgGoogle.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        ImageIcon finalGoogleIcon = new ImageIcon(imgGoogleScaled);
+        JButton btnNewButton = new JButton(finalGoogleIcon);
+        btnNewButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        btnNewButton.setBounds(123, 237, 89, 73);
+        contentPane.add(btnNewButton);
 		
 	}
 }
