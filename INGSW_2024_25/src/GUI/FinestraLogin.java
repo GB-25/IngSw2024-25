7d6fcc2e4d424916993ddc3a5a0270fc6f0f47de
GB-25 <gior.bozza@studenti.unina.it>
2025-01-23 20:06:44 +0100

    Nuova interfaccia FinestraLogin
    Pulita l'interfaccia per il login, con l'aggiunta dei pulsanti per l'accesso con facebook, google e github; possibilità di un panel show di immagini sul fondo della finestra

--- INGSW_2024_25/src/GUI/FinestraLogin.java
+++ INGSW_2024_25/src/GUI/FinestraLogin.java
@@ -4,10 +4,10 @@ import Class.Controller;
 import java.awt.*;
 
 import javax.swing.*;
-import com.jgoodies.forms.layout.FormLayout;
+/**import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.RowSpec; **/
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -16,6 +16,8 @@ public class FinestraLogin extends JFrame {
 
 	//private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 	
 	
 
@@ -49,13 +51,6 @@ public class FinestraLogin extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		JTextPane txtpnP = new JTextPane();
-		txtpnP.setForeground(new Color(0, 153, 255));
-		txtpnP.setFont(new Font("FreeSerif", Font.BOLD, 26));
-		txtpnP.setBounds(305, 25, 292, 73);
-		txtpnP.setBackground(new Color(255, 255, 255));
-		txtpnP.setText("            Benvenuto su\n           DietiEstates25");
-		contentPane.add(txtpnP);
 		
 		JLabel imageLabel = new JLabel();
         imageLabel.setBounds(33, 25, 246, 198); 
@@ -64,52 +59,91 @@ public class FinestraLogin extends JFrame {
         imageLabel.setIcon(new ImageIcon(image)); 
         contentPane.add(imageLabel);
         
-        JButton btnIscriviti = new JButton("Iscriviti");
-        btnIscriviti.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        	}
-        });
-        btnIscriviti.setForeground(new Color(0, 153, 255));
-        btnIscriviti.setBounds(420, 203, 117, 25);
-        contentPane.add(btnIscriviti);
-        
-        JButton btnAccedi = new JButton("Accedi");
-        btnAccedi.setForeground(new Color(0, 153, 255));
-        btnAccedi.setBounds(420, 285, 117, 25);
-        contentPane.add(btnAccedi);
-        
         JTextPane txtpnSeiGiIscritto = new JTextPane();
         txtpnSeiGiIscritto.setFont(new Font("FreeSerif", Font.BOLD, 16));
         txtpnSeiGiIscritto.setForeground(new Color(0, 153, 255));
-        txtpnSeiGiIscritto.setText("Sei già iscritto?");
-        txtpnSeiGiIscritto.setBounds(430, 250, 144, 21);
+        txtpnSeiGiIscritto.setText("Non sei iscritto?");
+        txtpnSeiGiIscritto.setBounds(408, 254, 144, 21);
         contentPane.add(txtpnSeiGiIscritto);
-        
-        JTextPane txtpnLaCasaDei = new JTextPane();
-        txtpnLaCasaDei.setFont(new Font("FreeSerif", Font.BOLD, 26));
-        txtpnLaCasaDei.setForeground(new Color(0, 153, 255));
-        txtpnLaCasaDei.setText("La casa dei tuoi sogni...");
-        txtpnLaCasaDei.setBounds(337, 95, 300, 35);
-        contentPane.add(txtpnLaCasaDei);
-        
-        JTextPane txtpnaPortataDi = new JTextPane();
-        txtpnaPortataDi.setForeground(new Color(0, 153, 255));
-        txtpnaPortataDi.setFont(new Font("FreeSerif", Font.BOLD, 26));
-        txtpnaPortataDi.setText("...A portata di mano!");
-        txtpnaPortataDi.setBounds(347, 142, 262, 49);
-        contentPane.add(txtpnaPortataDi);
         
         ImageIcon iconGoogle = new ImageIcon(getClass().getResource("/immagini/Google_logo.svg.png"));
         Image imgGoogle = iconGoogle.getImage();
-        Image imgGoogleScaled = imgGoogle.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        Image imgGoogleScaled = imgGoogle.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
         ImageIcon finalGoogleIcon = new ImageIcon(imgGoogleScaled);
-        JButton btnNewButton = new JButton(finalGoogleIcon);
-        btnNewButton.addActionListener(new ActionListener() {
+        JButton btnGoogle = new JButton(finalGoogleIcon);
+        btnGoogle.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
         	}
         });
-        btnNewButton.setBounds(123, 237, 89, 73);
+        btnGoogle.setBounds(341, 180, 78, 33);
+        contentPane.add(btnGoogle);
+        
+        ImageIcon iconFacebook = new ImageIcon(getClass().getResource("/immagini/facebook-logo.png"));
+        Image imgFacebook = iconFacebook.getImage();
+        Image imgFacebookScaled = imgFacebook.getScaledInstance(35, 25, Image.SCALE_SMOOTH);
+        ImageIcon finalFacebookIcon = new ImageIcon(imgFacebookScaled);
+        JButton btnFacebook = new JButton(finalFacebookIcon);
+        btnFacebook.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        btnFacebook.setBounds(506, 180, 78, 33);
+        contentPane.add(btnFacebook);
+        
+        ImageIcon iconGit = new ImageIcon(getClass().getResource("/immagini/gitlogo.png"));
+        Image imgGit = iconGit.getImage();
+        Image imgGitScaled = imgGit.getScaledInstance(25, 25, Image.SCALE_SMOOTH);
+        ImageIcon finalGitIcon = new ImageIcon(imgGitScaled);
+        JButton btnGit = new JButton(finalGitIcon);
+        btnGit.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        	
+        });
+        btnGit.setBounds(418, 180, 89, 33);
+        contentPane.add(btnGit);
+        
+        textField = new JTextField();
+        textField.setBounds(341, 83, 243, 20);
+        contentPane.add(textField);
+        textField.setColumns(10);
+        
+        textField_1 = new JTextField();
+        textField_1.setBounds(341, 115, 243, 20);
+        contentPane.add(textField_1);
+        textField_1.setColumns(10);
+        
+        JButton btnNewButton_3 = new JButton("Accedi");
+        btnNewButton_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+        btnNewButton_3.setForeground(new Color(0, 153, 255));
+        btnNewButton_3.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        	}
+        });
+        btnNewButton_3.setBounds(418, 146, 89, 23);
+        contentPane.add(btnNewButton_3);
+        
+        JButton btnNewButton = new JButton("ISCRIVITI ORA");
+        btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+        btnNewButton.setForeground(new Color(0, 153, 255));
+        btnNewButton.setBounds(408, 286, 124, 40);
         contentPane.add(btnNewButton);
+        
+        JTextPane txtpnLaCasaDei = new JTextPane();
+        txtpnLaCasaDei.setForeground(new Color(0, 153, 255));
+        txtpnLaCasaDei.setFont(new Font("Tahoma", Font.BOLD, 28));
+        txtpnLaCasaDei.setText("La casa dei tuoi sogni");
+        txtpnLaCasaDei.setBounds(10, 254, 388, 58);
+        contentPane.add(txtpnLaCasaDei);
+        
+        JTextPane txtpnAPortataDi = new JTextPane();
+        txtpnAPortataDi.setForeground(new Color(0, 153, 255));
+        txtpnAPortataDi.setFont(new Font("Tahoma", Font.BOLD, 28));
+        txtpnAPortataDi.setText("A portata di mano");
+        txtpnAPortataDi.setBounds(32, 323, 287, 58);
+        contentPane.add(txtpnAPortataDi);
 		
+        
+        
 	}
 }
