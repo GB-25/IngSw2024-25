dfccb759b0708c109d0ce792f51e4b2d690d0c73
GB-25 <gior.bozza@studenti.unina.it>
2025-02-04 09:58:51 +0100

    Revert "HttpServer"
    This reverts commit 5a55e9251c0a0f3907dd664dfe4d23b2b1d5f193.

--- INGSW_2024_25/src/Class/Controller.java
+++ INGSW_2024_25/src/Class/Controller.java
@@ -3,12 +3,7 @@ package Class;
 //import 
 import GUI.*;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class Controller extends HttpServlet {
+public class Controller {
 
 	//frame
 	
@@ -24,13 +19,6 @@ public class Controller extends HttpServlet {
 		finestraPrincipale.setVisible(true);
 	}
 	
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        // Imposta il contenuto della risposta come JSON
-        resp.setContentType("application/json");
-
-        // Scrivi la risposta (esempio: lista immobili)
-        resp.getWriter().println("{\"immobili\": [{\"titolo\": \"Appartamento\", \"prezzo\": 120000}]}");
-    }
 	
 	public static void main(String[] args)
 	{
