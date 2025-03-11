13aab3fafc91edca7ffc9f8bd6821f6487a2e90e
GB-25 <gior.bozza@studenti.unina.it>
2025-03-11 16:58:29 +0100

    storage
    babe wake up, it's time to add another .jar file

--- INGSW_2024_25/srcClient/Controller/Controller.java
+++ INGSW_2024_25/srcClient/Controller/Controller.java
@@ -77,7 +77,7 @@ public class Controller {
 	JFrame prenota;
 	JFrame visioneImmobile;
 	JFrame prenotazioneConfermata;
-	String ip = "192.158.31.40";
+	String ip = "34.76.2.59";
 	int porta = 12345;
 	private Map<String, List<Runnable>> notificheUtenti = new HashMap<>();
 	
