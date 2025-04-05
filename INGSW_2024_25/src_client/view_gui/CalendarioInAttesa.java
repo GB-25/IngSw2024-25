b5a9e7782128a7a496027ff0472cf24f027c192f
gennooo <157842567+gennooo@users.noreply.github.com>
2025-04-05 13:23:56 +0200

    PLEASE CONTROLLA SE E' TUTTO A POSTO
    love you jojo

--- INGSW_2024_25/src_client/view_gui/CalendarioInAttesa.java
+++ INGSW_2024_25/src_client/view_gui/CalendarioInAttesa.java
@@ -33,7 +33,7 @@ public class CalendarioInAttesa extends JFrame{
 	public CalendarioInAttesa(Controller c, User user, LocalDate selectedDateGlobal) {
 		setResizable(false);
 		FlatLaf.setup(new FlatLightLaf());
-		frame = new JPanel(new BorderLayout());
+		frame = new JPanel();
 		finestraCorrente = this;
 		setTitle("Visualizza appuntamenti pendenti");
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -41,8 +41,19 @@ public class CalendarioInAttesa extends JFrame{
 		setSize(new Dimension(500, 450));
 		setContentPane(frame);
 		
-		JPanel indietroPanel = new JPanel(new BorderLayout());
+		JButton logoButton = createIconButton("/immagini/logopngwhite.png", 200, 100);
+
+        JPanel logoPanel = new JPanel();
+        logoPanel.setBounds(0, 0, 486, 82);
+        logoPanel.setBackground(new Color(40, 132, 212));
+        logoPanel.setLayout(null);
+        logoPanel.add(logoButton);
+        frame.add(logoPanel, BorderLayout.NORTH);
+		
+		JPanel indietroPanel = new JPanel();
+		indietroPanel.setBounds(0, 80, 486, 32);
         JButton indietroButton = new JButton("←");
+        indietroButton.setBounds(0, 0, 44, 26);
         indietroButton.setHorizontalTextPosition(SwingConstants.LEFT);
         indietroButton.setBackground(Color.WHITE);
         indietroButton.setFont(new Font("Helvetica", Font.BOLD, 13));
@@ -52,9 +63,11 @@ public class CalendarioInAttesa extends JFrame{
         	dispose();
         	c.viewCalendar(finestraCorrente, user);
         });
-        indietroPanel.add(indietroButton, BorderLayout.WEST);
+        indietroPanel.setLayout(null);
+        indietroPanel.add(indietroButton);
 
-		JPanel reservationsPanel = new JPanel (new BorderLayout());
+		JPanel reservationsPanel = new JPanel ();
+		reservationsPanel.setBounds(0, 111, 486, 302);
 		
 
 		prenotazioni = (ArrayList<Prenotazione>) c.getPrenotazione(user, selectedDateGlobal);
@@ -70,8 +83,12 @@ public class CalendarioInAttesa extends JFrame{
 		JList<String> reservationsList = new JList<>(reservations.toArray(new String[0]));
 		reservationsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		JScrollPane scrollPane = new JScrollPane(reservationsList);
-		reservationsPanel.add(new JLabel("Prenotazioni per il " + prenotazione.getDataPrenotazione()), BorderLayout.NORTH);
-		reservationsPanel.add(scrollPane, BorderLayout.CENTER);
+		scrollPane.setBounds(0, 13, 486, 374);
+		reservationsPanel.setLayout(null);
+		JLabel label = new JLabel("Prenotazioni per il " + prenotazione.getDataPrenotazione());
+		label.setBounds(0, 0, 486, 13);
+		reservationsPanel.add(label);
+		reservationsPanel.add(scrollPane);
 		
 		reservationsList.addMouseListener(new MouseAdapter() {
 		    
@@ -99,8 +116,23 @@ public class CalendarioInAttesa extends JFrame{
 		        }
 		    }
 		});
-		frame.add(indietroPanel, BorderLayout.NORTH);
-		frame.add(reservationsPanel, BorderLayout.CENTER);
+		frame.setLayout(null);
+		frame.add(indietroPanel);
+		frame.add(reservationsPanel);
 		setVisible(true);
 	}
+	
+	private JButton createIconButton(String path, int width, int height) {
+        ImageIcon icon = new ImageIcon(getClass().getResource(path));
+        Image image = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        JButton button = new JButton(new ImageIcon(image));
+        button.setBounds(129, -10, 232, 108);
+
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setOpaque(false);
+
+        return button;
+    }
 }
