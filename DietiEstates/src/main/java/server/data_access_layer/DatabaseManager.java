5316f291843aebd57c638c4b814646b72d921317
GB-25 <121553232+GB-25@users.noreply.github.com>
2025-04-12 13:36:12 +0200

    Update DatabaseManager.java

--- DietiEstates/src/main/java/server/data_access_layer/DatabaseManager.java
+++ DietiEstates/src/main/java/server/data_access_layer/DatabaseManager.java
@@ -83,7 +83,7 @@ public class DatabaseManager implements UserRepositoryInterface, HouseRepository
             stmt.executeUpdate();
            
         } catch (SQLException e) {
-            logger.severe("Errore in register, DB: " + e.getMessage()););
+            logger.severe("Errore in register, DB: " + e.getMessage());
         }
     }
     
